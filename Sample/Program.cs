0dff77c750877dfc4ec43f2a487e08e0b193236a
Andrew Stakhov <andrew.stakhov@gmail.com>
2021-05-05 17:30:56 -0400

    Initial

---
+++ Sample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Versioning;
+
+namespace JsonEmbeddingGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+                   
+            Console.WriteLine(EmbeddedResources.sample_json);
+        }
+    }
+} 
+  
